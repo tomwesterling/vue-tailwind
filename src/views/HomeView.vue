044b6618668606301c4e1573d9530ff9f5526223
tomwesterling <tom.westerling@web.de>
2023-11-05 18:23:37 +0100

    init commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="p-6 max-w-md mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div class="shrink-0 bg-black">
+    <p class="text-white">Huhu</p>
   </div>
+  <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
+</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
   }
 }
 </script>
+
+<style scoped>
+
+</style>
